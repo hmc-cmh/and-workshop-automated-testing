e27bc4f7ab9de0fd2687b9129fd8c15147b1c799
Hattie Stamp <hstamp@and.digital>
2019-01-30 13:39:57 +0000

    trying hooks

--- lab-2-improve-code/src/libs.js
+++ lab-2-improve-code/src/libs.js
@@ -1,60 +1,38 @@
-var out = new Object();
+let out = new Object();
 
 out = {
-  ['req-one']: req_ONE,
+  'req-one': req_ONE,
 };
 
-
-function req_ONE(g)
-{const r = "Hello";
+function req_ONE(g) {
+  const r = 'Hello';
 
   const m = [r, g].join(', ');
 
-  return m + ".";
+  return `${m}.`;
 }
 
+const functionForRequirementThree_SHOUT = (SHOUT = 'my fried') => {
+  const theReturnString = `H${
+    /^[A-Z ]+$/.test(SHOUT) ? 'ELLO' : 'ello'
+  }, ${SHOUT}${/^[A-Z ]+$/.test(SHOUT) ? '!' : '.'}`;
+  return theReturnString;
+};
 
-
-
-
-const functionForRequirementThree_SHOUT=(SHOUT='my fried')=>{
-  const theReturnString = `H${/^[A-Z ]+$/.test(SHOUT)?'ELLO':"ello"}, ${SHOUT}${/^[A-Z ]+$/.test(SHOUT)?'!':'.'}`
-  return theReturnString;}
-
-
-
-
-
-
-
-
-
-
-
-
-let foo = (NAMESTRINGNOTNULL) =>
-{ r = "Hello";
+const foo = (NAMESTRINGNOTNULL) => {
+  r = 'Hello';
   // AMAZING TRICK TYPOF OF and NULL, WOOP WOOP!
-  if (typeof NAMESTRINGNOTNULL === 'object') return "Hello, my friend.";;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  if (typeof NAMESTRINGNOTNULL === 'object') return 'Hello, my friend.';
 
   const m = [r, NAMESTRINGNOTNULL].join(', ');
 
-  return m
-    +
-    ".";
-}
-
-
-out = {
-
-  ...out,
-  ['req-TWO']: foo,
-  ['req-3']: functionForRequirementThree_SHOUT,
+  return `${m}.`;
 };
 
+out = {
+  ...out,
+  'req-TWO': foo,
+  'req-3': functionForRequirementThree_SHOUT,
+};
 
-
-
-
-
-module.exports = out
+module.exports = out;
